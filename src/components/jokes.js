19f92d008d3a588656bb342e0cb06d158e16ba8f
vinaybvk7 <39928115+vinaybvk7@users.noreply.github.com>
2018-06-04 22:49:43 -0400

    Update jokes.js

--- src/components/jokes.js
+++ src/components/jokes.js
@@ -3,14 +3,14 @@ import React from 'react';
 const jokes = props =>{
 
 	return(<div className="content">
-				<div className="card">
-					<div className="card-content">
+				<div className="board">
+					<div className="board-content">
 					<p className="subtitle">
 						{ props.currentJoke && props.currentJoke.joke}
 					</p>
 				</div>
-				<footer className="card-footer">
-					<div className={ props.currentJoke && props.currentJoke.funny === true ? "card-footer-item funny" : "card-footer-item "} onClick={()=>{props.reactOnJoke(true, props.currentJoke )}}>
+				<footer className="board-footer">
+					<div className={ props.currentJoke && props.currentJoke.funny === true ? "board-footer-item funny" : "board-footer-item "} onClick={()=>{props.reactOnJoke(true, props.currentJoke )}}>
 						<span className="icon is-small">
 							<i className="fa fa-chain"></i>
 						</span>
@@ -18,7 +18,7 @@ const jokes = props =>{
 							Funny
 						</span>
 					</div>
-					<div className={props.currentJoke && props.currentJoke.funny === false ? "card-footer-item not-funny" : "card-footer-item"} onClick={()=>{props.reactOnJoke(false, props.currentJoke )}}>
+					<div className={props.currentJoke && props.currentJoke.funny === false ? "board-footer-item not-funny" : "board-footer-item"} onClick={()=>{props.reactOnJoke(false, props.currentJoke )}}>
 						<span className="icon is-small">
 							<i className="fa fa-twitter"></i>
 						</span>
